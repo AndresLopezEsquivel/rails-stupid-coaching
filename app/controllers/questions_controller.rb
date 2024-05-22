d41f9234d1d054cd022c51a30f2360c383fe71b4
Andres Lopez Esquivel <lopezesquivelandres1@gmail.com>
2024-05-21 20:44:01 -0600

    Refactor code

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,12 +4,16 @@ class QuestionsController < ApplicationController
 
   def answer
     @question = params[:question]
-    if @question == "I am going to work right now!"
-      @answer = ""
-    elsif @question.include? "?"
-      @answer = "Silly question, get dressed and go to work!"
+    @answer = ask_to_coach(@question)
+  end
+
+  def ask_to_coach(question)
+    if question == "I am going to work right now!"
+      ""
+    elsif question.include? "?"
+      "Silly question, get dressed and go to work!"
     else
-      @answer = "I don't care, get dressed and go to work!"
+      "I don't care, get dressed and go to work!"
     end
   end
 end
